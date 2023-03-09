528109ea621727ffe7b4d07fe801c57a0100124c
Danisole <danisole82@gmail.com>
2023-03-09 20:40:18 -0300

    operaciones arreglo

--- matematicas.py
+++ matematicas.py
@@ -6,11 +6,11 @@ if len(sys.argv)==3:
 
     sengundonum = int(sys.argv[2])
 
-    print(f"la suma de {primernum} y {sengundonum} es igual {primernum+sengundonum}") 
-    print(f"la resta de {primernum} y {sengundonum} es igual {primernum-sengundonum}") 
-    print(f"la multiplicacion de {primernum} y {sengundonum} es igual {primernum*sengundonum}") 
-    print(f"la division de {primernum} y {sengundonum} es igual {primernum/sengundonum:.2f}") 
-    print(f"la potencia de {primernum} y {sengundonum} es igual {primernum**sengundonum}")    
+    print(f"la suma de {primernum} y {sengundonum} es igual {sumar(primernum,sengundonum)}") 
+    print(f"la resta de {primernum} y {sengundonum} es igual {restar(primernum,sengundonum)}") 
+    print(f"la multiplicacion de {primernum} y {sengundonum} es igual {multiplicar(primernum,sengundonum)}") 
+    print(f"la division de {primernum} y {sengundonum} es igual {dividir(primernum,sengundonum):.2f}") 
+    print(f"la potencia de {primernum} y {sengundonum} es igual {potencia(primernum,sengundonum)}")    
     print(f" mi num de la suerte es {py}")
 
 
